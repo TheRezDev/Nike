b95b0ec77a400c9192221d1a80f03332e7ae9332
Reza Yousefi <32484598+TheRezDev@users.noreply.github.com>
2023-11-01 14:12:52 +0330

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <div className='bg-slate-800 font-bold t text-gray-200 flex justify-center h-52'>
+      <p className="item-center justify-center">hello world </p>
+    </div>
+  )
+}
+
+export default App

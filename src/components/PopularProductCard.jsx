6d75d4482de1294734119dc50ef8ce79b91ee553
Reza Yousefi <32484598+TheRezDev@users.noreply.github.com>
2023-11-08 13:13:26 +0330

    revised popular products card section

--- src/components/PopularProductCard.jsx
+++ src/components/PopularProductCard.jsx
@@ -10,7 +10,7 @@ const PopularProductCard = ({
          <img
             src={imgURL}
             alt={name}
-            className="w-[280px] h-[280px]"
+            className="w-[282px] h-[282px]"
          />
          <div className="mt-8 flex justify-start gap-2.5">
             <img
@@ -22,13 +22,14 @@ const PopularProductCard = ({
             <p className="font-montserrat text-xl leading-normal text-slate-gray ">
                (4.5)
             </p>
-            <h3 className="mt-2 text-2xl leading-normal font-semibold font-palanquin">
-               {name}
-            </h3>
-            <p className="mt-2 font-semibold font-montserrat text-coral-red text-2xl leading-normal">
-               {price}
-            </p>
          </div>
+
+         <h3 className="mt-2 text-2xl leading-normal font-semibold font-palanquin">
+            {name}
+         </h3>
+         <p className="mt-2 font-semibold font-montserrat text-coral-red text-2xl leading-normal">
+            {price}
+         </p>
       </div>
    );
 };
